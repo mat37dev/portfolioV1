3cd804413763bcb9e2f59acf4bdeb75fb46f144c
mat37dev <mathieu.crosnier15@outlook.fr>
2022-05-31 21:17:31 +0200

    Site finie Animations En cours

---
+++ javaScript/script.js
@@ -0,0 +1,30 @@
+/* Animation */
+var typed = new Typed(".type", {
+    strings: ["","Etudiant En Développement", "Développeur Amateur", "Développeur Jeux-Vidéos"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
+/* Cote */
+const nav = document.querySelector(".nav"),
+    navList = nav.querySelectorAll("li"),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll(".section"),
+    totalSection = allSection.length;
+for (let i=0; i<totalNavList; i++){
+    const a = navList[i].querySelector("a");
+    a.addEventListener("click", function(){
+        for (let j=0; j<totalNavList; j++){
+            navList[j].querySelector("a").classList.remove("active");
+        }
+        this.classList.add("active")
+        showSection(this);
+    })
+}
+function showSection(element){
+    for (let i=0;i<totalSection;i++){
+        allSection[i].classList.remove("active");
+    }
+    const target = element.getAttribute("href").split("#")[1];
+    document.querySelector("#" + target).classList.add("active")
+}
