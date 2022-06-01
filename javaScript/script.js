42fcd13790252119b54af9312ec98a0331d3c5aa
mat37dev <mathieu.crosnier15@outlook.fr>
2022-06-01 10:13:00 +0200

    V1.0: - Site Finie (sauf données)

--- javaScript/script.js
+++ javaScript/script.js
@@ -14,18 +14,27 @@ const nav = document.querySelector(".nav"),
 for (let i=0; i<totalNavList; i++){
     const a = navList[i].querySelector("a");
     a.addEventListener("click", function(){
-        for (let i=0;i<totalSection;i++){
-            allSection[i].classList.remove("back-section");
-        }
+        removeBackSection();
         for (let j=0; j<totalNavList; j++){
             if(navList[j].querySelector("a").classList.contains("active")){
-                allSection[j].classList.add("back-section");
+                addBackSection(j);
             }
             navList[j].querySelector("a").classList.remove("active");
         }
         this.classList.add("active")
         showSection(this);
+        if(window.innerWidth <1200){
+            coteSectionTogglerBtn();
+        }
     })
+}
+function addBackSection(nb){
+    allSection[nb].classList.add("back-section");
+}
+function removeBackSection(){
+    for (let i=0;i<totalSection;i++){
+        allSection[i].classList.remove("back-section");
+    }
 }
 function showSection(element){
     for (let i=0;i<totalSection;i++){
@@ -34,6 +43,31 @@ function showSection(element){
     const target = element.getAttribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active")
 }
+function updateNav(element){
+    for (let i=0;i<totalNavList;i++){
+        navList[i].querySelector("a").classList.remove("active");
+        const target = element.getAttribute("href").split("#")[1];
+        if(target === navList[i].querySelector("a").getAttribute("href").split("#")[1]){
+            navList[i].querySelector("a").classList.add("active");
+        }
+    }
+}
+/* Boutons de Redirections */
+document.querySelector(".hire-me").addEventListener("click", function (){
+    buttonRedirect(this);
+})
+document.querySelector(".btn-about").addEventListener("click", function (){
+    buttonRedirect(this);
+})
+function buttonRedirect(element){
+    const sectionIndex = element.getAttribute("data-section-index");
+    showSection(element);
+    updateNav(element);
+    removeBackSection();
+    addBackSection(sectionIndex);
+}
+
+/* Clique sur le burger */
 const navTogglerBtn = document.querySelector(".nav-toggler"),
     cote = document.querySelector(".cote");
 navTogglerBtn.addEventListener("click",()=>{
@@ -41,4 +75,8 @@ navTogglerBtn.addEventListener("click",()=>{
 })
 function coteSectionTogglerBtn(){
     cote.classList.toggle("open");
+    navTogglerBtn.classList.toggle("open");
+    for (let i=0;i<totalSection;i++){
+        allSection[i].classList.toggle("open");
+    }
 }
